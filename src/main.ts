1b6f62a709c4bcb4d1bd62ebe5bd6b049f410062
yauheni.s <yauheni.s@spur-i-t.com>
2023-10-01 12:43:07 +0200

    add code formatting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  console.log('process.env.PORT = ', process.env);
+  await app.listen(process.env.PORT);
 }
+
 bootstrap();
